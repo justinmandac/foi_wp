12036d2e4b64f30bf1c66bbacfd290d9181e62cf
unknown <justinmandac@gmail.com>
2015-07-26 22:04:43 +0800

    polish

--- footer.php
+++ footer.php
@@ -2,12 +2,13 @@
       <div class="footer-content">
             <div class="left-block">
               <p>
-Praesent luctus consequat enim molestie dignissim. Nulla efficitur quam in purus vestibulum egestas. Integer vel magna est. Phasellus et finibus magna, sed pulvinar nibh. Nunc vitae viverra sapien, in tempus risus. Aliquam justo ipsum, euismod eu massa a, porta dictum risus. Duis non hendrerit augue. Pellentesque ligula magna, commodo quis leo ut, eleifend scelerisque diam. Suspendisse potenti. Ut at libero ornare, viverra lacus eget, varius nulla. Donec placerat mauris sed diam gravida euismod. Mauris consectetur vulputate nibh, vitae luctus nulla efficitur nec                
               </p>
+              <div class="bottom-menu">
+                  <?php wp_nav_menu( array( 'theme_location' => 'footer-nav' ) ); ?>
+              </div>
             </div>
             <div class="right-block">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius volutpat tortor, ut dapibus magna tempor sit amet. Aliquam sed finibus nibh. Pellentesque sed massa erat. Maecenas eu dui ac urna lacinia venenatis. Fusce maximus lacinia placerat. Quisque vitae leo nunc. Integer et vehicula mi, in accumsan sapien.
               </p>
             </div>
       </div>
