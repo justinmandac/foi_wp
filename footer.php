6648aafc2072ab9a0947a7f121fe6c7c53dadaf7
unknown <justinmandac@gmail.com>
2015-08-04 02:36:31 +0800

    footer layout; added footer logo and copyright

--- footer.php
+++ footer.php
@@ -1,15 +1,20 @@
     <footer class="footer-wrapper">
       <div class="footer-content container">
             <div class="left-block">
-              <p>
-              </p>
               <div class="bottom-menu">
                   <?php wp_nav_menu( array( 'theme_location' => 'footer-nav' ) ); ?>
               </div>
             </div>
             <div class="right-block">
-              <p>
-              </p>
+
+            </div>
+            <div class="full-block">
+              <div class="copyright">
+                <div class="footer-logo" style="background-image:url('<?php echo get_template_directory_uri().'/images/spritesheet.png' ?>')">
+
+                </div>
+                <span> &copy; Institute of Freedom For Information</span>
+              </div>
             </div>
       </div>
       <?php wp_footer(); ?>
