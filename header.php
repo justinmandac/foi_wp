59f92698c888267af59ba32f28029960ecb0f986
unknown <justinmandac@gmail.com>
2015-08-21 22:50:19 +0800

    viewport scaling

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta name="name" content="content">
     <meta charset="<?php bloginfo('charset');?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
