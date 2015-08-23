11e1697785ea8c20ec93bb8d444d2cd364836736
unknown <justinmandac@gmail.com>
2015-08-23 11:31:09 +0800

    code refactoring

--- js/tracker.js
+++ js/tracker.js
@@ -218,87 +218,177 @@ var predef = [
   }
 ];
 
-function sortByDate(a,b) {
-  var aDate = a.date;
-  var bDate = b.date;
-  return (moment(aDate).isBefore(bDate)) ? -1 : (moment(aDate).isAfter(bDate) ? 1: 0);
-}
+var tools = {};
 
-function mergeSigEnt(sig, ent) {
-  sig.content = ent.content;
-  sig.uri     = ent.uri;
-  sig.rating  = ent.rating;
-  sig.title   = ent.title;
+(function(context){
 
-  return sig;
-}
+  var timeline_options = {};
+  var render_options = {};
 
-function buildModel(data) {
-  "use strict"
-  var arr = [];
+  context.init = function(timeline_opts, render_opts) {
+    timeline_options = timeline_opts;
+    render_options =   render_opts;
 
-  data.forEach(function(val) {
-    var title = val.title;
-    var guid  = val.link;
-    var html  = document.createElement('div');
-    html.innerHTML = val.encoded;
-    var attr = html.lastElementChild;
-    var content = html.children[0];
-    var guid = val.link;
+  }
+  /* PRIVATE FUNCTIONS */
+  /*
+    Merges 'significant data' ie. congress deadlines with entry blocks with coincinding dates
+  */
+  function mergeSigEnt(sig, ent) {
+    sig.content = ent.content;
+    sig.uri     = ent.uri;
+    sig.rating  = ent.rating;
+    sig.title   = ent.title;
 
-    var post = {
-      type: 'ent',
-      title: title,
-      date: moment(attr.children[0].innerHTML, 'MM-DD-YYYY'),
-      date_flag: null,
-      rating: parseInt(attr.children[1].innerHTML),
-      sig: null,
-      subs: null,
-      content: content,
-      uri: guid
+    return sig;
+  };
 
-    };
+  function sortByDate(a, b) {
+    var aDate = a.date;
+    var bDate = b.date;
+    return (moment(aDate).isBefore(bDate)) ? -1 : (moment(aDate).isAfter(bDate) ? 1: 0);
+  };
 
-    arr.push(post);
+  function renderSigBlock(text) {
+    //stuff to be re used
+    var excon  = document.createElement('i');
+    //create sig-block
+    var sig_block     = document.createElement('div');
+    var sig_icon_div  = document.createElement('div');
+    var sig_text_div  = document.createElement('div');
+    var sig_text      = document.createElement('p');
 
-  });
+    sig_block.className = 'sig-block';
 
-  return arr;
-}
+    excon.className = 'fa fa-exclamation-circle';
+    sig_icon_div.appendChild(excon);
+    sig_icon_div.className = 'icon';
 
-function mergeSimilarModels(data) {
-  var merge = [];
+    sig_text_div.className = 'text';
+    sig_text.appendChild(document.createTextNode(text));
+    sig_text_div.appendChild(sig_text);
 
-  while(data.length > 0) {
-    var bef = data.pop();
-    var aft = data.pop();
+    sig_block.appendChild(sig_icon_div);
+    sig_block.appendChild(sig_text_div);
 
-    if(data.length == 3) {
-      //special case for even numbered arrays
-      var last = data.pop();
+    return sig_block;
+  };
 
-      if(bef.type == aft.type) {
-        if( aft.type == last.type) {
-          //no merging for similar items
-          merge.push(bef);
-          merge.push(aft);
-          merge.push(last);
-        }
-        else {
-          //since they're all sorted in order and only two similar dates may exist,
-          //bef will never be equal to last in terms of date
-          if(aft.date._i == last.date._i) {
-            bef.type == 'sig'?  merge.push(mergeSigEnt(aft,last)) : merge.push(mergeSigEnt(last,aft));
-          }
-          else { //unequal dates, push
+  function renderPostBlock(title, index) {
+    //stuff to be re used
+    'use strict'
+    var eyecon = document.createElement('i');
+
+    var post_block = document.createElement('div');
+    var post_icon_div = document.createElement('div');
+    var post_text_div = document.createElement('div');
+    var post_text     = document.createElement('p');
+
+    post_block.className = 'post-block';
+    post_block.setAttribute('data-index', index);
+    eyecon.className = 'fa fa-eye';
+    post_icon_div.appendChild(eyecon);
+    post_icon_div.className = 'icon';
+
+    post_text_div.className = "text";
+    post_text.appendChild(document.createTextNode(title));
+    post_text_div.appendChild(post_text);
+
+    post_block.appendChild(post_icon_div);
+    post_block.appendChild(post_text_div);
+
+
+    return post_block;
+  };
+
+  /*PUBLIC FUNCTIONS*/
+
+  context.buildModel = function(data) {
+    "use strict"
+    var arr = [];
+
+    data.forEach(function(val) {
+      var title = val.title;
+      var guid  = val.link;
+      var html  = document.createElement('div');
+      html.innerHTML = val.encoded;
+      var attr = html.lastElementChild;
+      var content = html.children[0];
+      var guid = val.link;
+
+      var post = {
+        type: 'ent',
+        title: title,
+        date: moment(attr.children[0].innerHTML, 'MM-DD-YYYY'),
+        date_flag: null,
+        rating: parseInt(attr.children[1].innerHTML),
+        sig: null,
+        subs: null,
+        content: content,
+        uri: guid
+
+      };
+
+      arr.push(post);
+
+    });
+
+    return arr;
+  };
+
+  context.mergeSimilarModels = function(data) {
+    var merge = [];
+
+    //SORT HERE
+    data.sort(sortByDate);
+
+    while(data.length > 0) {
+      var bef = data.pop();
+      var aft = data.pop();
+
+      if(data.length == 3) {
+        //special case for even numbered arrays
+        var last = data.pop();
+
+        if(bef.type == aft.type) {
+          if( aft.type == last.type) {
+            //no merging for similar items
+            merge.push(bef);
             merge.push(aft);
             merge.push(last);
           }
+          else {
+            //since they're all sorted in order and only two similar dates may exist,
+            //bef will never be equal to last in terms of date
+            if(aft.date._i == last.date._i) {
+              bef.type == 'sig'?  merge.push(mergeSigEnt(aft,last)) : merge.push(mergeSigEnt(last,aft));
+            }
+            else { //unequal dates, push
+              merge.push(aft);
+              merge.push(last);
+            }
+          }
+        }
+        else {
+          //if bef == aft, bef can never be equal to last. check if bef should be merged
+          //with aft
+          if(bef.type == aft.type) { //similar types cannot be merged. push
+            merge.push(bef);
+            merge.push(aft);
+          }
+          else {
+            if(bef.date._i == aft.date._i) {
+              bef.type == 'sig'?  merge.push(mergeSigEnt(bef,aft)) : merge.push(mergeSigEnt(aft,bef));
+            }
+            else { //unequal dates, push
+              merge.push(bef);
+              merge.push(aft);
+            }
+          }
+          merge.push(last);
         }
       }
       else {
-        //if bef == aft, bef can never be equal to last. check if bef should be merged
-        //with aft
         if(bef.type == aft.type) { //similar types cannot be merged. push
           merge.push(bef);
           merge.push(aft);
@@ -312,195 +402,125 @@ function mergeSimilarModels(data) {
             merge.push(aft);
           }
         }
-        merge.push(last);
       }
     }
-    else {
-      if(bef.type == aft.type) { //similar types cannot be merged. push
-        merge.push(bef);
-        merge.push(aft);
+
+    return merge;
+  };
+
+  context.renderTimeline = function(data) {
+
+    var target = timeline_options.wrapper.removeChild(timeline_options.target);
+    var indices = [];
+    var tracker_count = 0;
+
+    data.forEach(function(val){
+      var li = document.createElement('li');
+      var heading_text = document.createElement('h5');
+      var item_content_template = '';
+      var point_content = document.createElement('div');
+
+      li.className+='point ';
+      point_content.className+='point-content ';
+      heading_text.className+='point-title ';
+      if(val.type == 'sig') {
+
+        li.className+='sig ';
+
+        heading_text.appendChild(document.createTextNode('! '));
+        if(val.date_flag) {
+          heading_text.appendChild(document.createTextNode(val.date.format('MMM YYYY')));
+        }
+        else { //render full date ie mm-dd
+          heading_text.appendChild(document.createTextNode(val.date.format('MMM DD, YYYY')));
+
+        }
+
+        if(val.sig !== 'none') {
+          heading_text.appendChild(document.createTextNode(':'+val.sig));
+        }
+
+        var subs = val.subs;
+
+        subs.forEach(function(sub_items) {
+          point_content.appendChild(renderSigBlock(sub_items));
+        });
+
       }
       else {
-        if(bef.date._i == aft.date._i) {
-          bef.type == 'sig'?  merge.push(mergeSigEnt(bef,aft)) : merge.push(mergeSigEnt(aft,bef));
+
+        indices.push(tracker_count);
+
+        heading_text.appendChild(document.createTextNode(val.date.format('MMM DD')));
+
+        li.onclick = function() {
+          context.renderContent(val);
+        };
+
+        point_content.appendChild(renderPostBlock(val.title, tracker_count));
+
+
+      }
+      tracker_count++;
+      li.appendChild(heading_text);
+      li.appendChild(point_content);
+
+      timeline_options.target.appendChild(li);
+    });
+
+    timeline_options.wrapper.appendChild(target);
+
+    return indices;
+  };
+
+  context.renderContent = function(data) {
+    var date  = document.createElement('h4');
+    var title = document.createElement('h3');
+    var guid = document.createElement('a');
+    var cont_link = document.createElement('a');
+    //build upper title block
+    date.appendChild(document.createTextNode(data.date.format('MMMM DD, YYYY')));
+
+    cont_link.href = guid.href = data.uri;
+    cont_link.targer = guid.target = '_blank';
+    guid.appendChild(document.createTextNode(data.title));
+
+    title.appendChild(guid);
+
+    render_options.title.innerHTML = '';
+    render_options.info.innerHTML = '';
+
+    render_options.title.appendChild(date);
+
+    render_options.title.appendChild(title);
+
+    //cont_link.appendChild(data.content);
+
+    render_options.info.appendChild(data.content);
+
+    //build read more link
+    render_options.readmore.href = data.uri;
+
+    render_options.gauge.highcharts(Highcharts.merge(gaugeOptions, {
+      series: [{
+        name: 'Performance',
+        data: [parseInt(data.rating)],
+        dataLabels: {
+          format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+          ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>'
+        },
+        tooltip: {
+          valueSuffix: null
         }
-        else { //unequal dates, push
-          merge.push(bef);
-          merge.push(aft);
-        }
-      }
-    }
-  }
+      }]
 
-  return merge;
-}
+    }));
+    //hide the rotating cog + text
+    render_options.loader.style.display = 'none';
+  };
 
-function renderSigBlock(text) {
-  //stuff to be re used
-  var excon  = document.createElement('i');
-  //create sig-block
-  var sig_block     = document.createElement('div');
-  var sig_icon_div  = document.createElement('div');
-  var sig_text_div  = document.createElement('div');
-  var sig_text      = document.createElement('p');
+})(tools);
 
-  sig_block.className = 'sig-block';
-
-  excon.className = 'fa fa-exclamation-circle';
-  sig_icon_div.appendChild(excon);
-  sig_icon_div.className = 'icon';
-
-  sig_text_div.className = 'text';
-  sig_text.appendChild(document.createTextNode(text));
-  sig_text_div.appendChild(sig_text);
-
-  sig_block.appendChild(sig_icon_div);
-  sig_block.appendChild(sig_text_div);
-
-  return sig_block;
-}
-
-function renderPostBlock(title, index, opts) {
-  //stuff to be re used
-  'use strict'
-  var eyecon = document.createElement('i');
-
-  var post_block = document.createElement('div');
-  var post_icon_div = document.createElement('div');
-  var post_text_div = document.createElement('div');
-  var post_text     = document.createElement('p');
-
-  post_block.className = 'post-block';
-  post_block.setAttribute('data-index', index);
-  eyecon.className = 'fa fa-eye';
-  post_icon_div.appendChild(eyecon);
-  post_icon_div.className = 'icon';
-
-  post_text_div.className = "text";
-  post_text.appendChild(document.createTextNode(title));
-  post_text_div.appendChild(post_text);
-
-  post_block.appendChild(post_icon_div);
-  post_block.appendChild(post_text_div);
-
-
-  return post_block;
-}
-
-function renderTimeline(opts, data, render_opts) {
-  //opts.target.detach();
-
-  var target = opts.wrapper.removeChild(opts.target);
-  var indices = [];
-  var tracker_count = 0;
-  //console.log(target);
-
-  data.forEach(function(val){
-
-
-    var li = document.createElement('li');
-    var heading_text = document.createElement('h5');
-    var item_content_template = '';
-    var point_content = document.createElement('div');
-
-    li.className+='point ';
-    point_content.className+='point-content ';
-    heading_text.className+='point-title ';
-    if(val.type == 'sig') {
-
-      li.className+='sig ';
-
-      heading_text.appendChild(document.createTextNode('! '));
-      if(val.date_flag) {
-        heading_text.appendChild(document.createTextNode(val.date.format('MMM YYYY')));
-      }
-      else { //render full date ie mm-dd if false
-        heading_text.appendChild(document.createTextNode(val.date.format('MMM DD, YYYY')));
-
-      }
-
-      if(val.sig !== 'none') {
-        heading_text.appendChild(document.createTextNode(':'+val.sig));
-      }
-
-      var subs = val.subs;
-
-      subs.forEach(function(sub_items) {
-        point_content.appendChild(renderSigBlock(sub_items));
-      });
-
-    }
-    else {
-
-      indices.push(tracker_count);
-
-      heading_text.appendChild(document.createTextNode(val.date.format('MMM DD')));
-
-      li.onclick = function() {
-        renderContent(val, render_opts);
-      };
-
-      point_content.appendChild(renderPostBlock(val.title, tracker_count));
-
-
-    }
-    tracker_count++;
-    li.appendChild(heading_text);
-    li.appendChild(point_content);
-
-    opts.target.appendChild(li);
-  });
-
-  opts.wrapper.appendChild(target);
-
-  return indices;
-}
-
-function renderContent(data, opts ) {
-  var date  = document.createElement('h4');
-  var title = document.createElement('h3');
-  var guid = document.createElement('a');
-  var cont_link = document.createElement('a');
-  //build upper title block
-  date.appendChild(document.createTextNode(data.date.format('MMMM DD, YYYY')));
-
-  cont_link.href = guid.href = data.uri;
-  cont_link.targer = guid.target = '_blank';
-  guid.appendChild(document.createTextNode(data.title));
-
-  title.appendChild(guid);
-
-  opts.title.innerHTML = '';
-  opts.info.innerHTML = '';
-
-  opts.title.appendChild(date);
-
-  opts.title.appendChild(title);
-
-  //cont_link.appendChild(data.content);
-
-  opts.info.appendChild(data.content);
-
-  //build read more link
-  opts.readmore.href = data.uri;
-
-  opts.gauge.highcharts(Highcharts.merge(gaugeOptions, {
-    series: [{
-      name: 'Performance',
-      data: [parseInt(data.rating)],
-      dataLabels: {
-        format: '<div style="text-align:center"><span style="font-size:25px;color:' +
-        ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>'
-      },
-      tooltip: {
-        valueSuffix: null
-      }
-    }]
-
-  }));
-  //hide the rotating cog + text
-  opts.loader.style.display = 'none';
-}
 
 (function($) {
   $(function(){
@@ -520,30 +540,27 @@ function renderContent(data, opts ) {
 
     var triggers = document.getElementsByClassName('timeline-trigger');
 
+    tools.init(timeline_opts, render_opts);
+
+
     function feed_success(obj) {
       var tracker_json = $.xml2json(obj).channel.item;
-      //var tracker_json = obj.channel.item;
 
+      var data = tools.mergeSimilarModels(predef.concat(tools.buildModel(tracker_json)));
 
-      var data = mergeSimilarModels(predef.concat(buildModel(tracker_json)).sort(sortByDate));
-
-      var indices = renderTimeline(timeline_opts, data, render_opts);
+      var indices = tools.renderTimeline(data);
       var first_index = indices[0];
 
-      renderContent(data[first_index], render_opts);
+      tools.renderContent(data[first_index]);
 
       $('.timeline-trigger').sidr({
         displace: false,
         onOpen: function () {
-
            for(var x = 0 ; x < triggers.length; x++) {
              triggers[x].className+='sidr-open';
            }
-
         },
         onClose: function() {
-
-
           for(var x = 0 ; x < triggers.length; x++) {
             triggers[x].className = triggers[x].className.replace(/sidr-open/g, '');
           }
@@ -551,8 +568,6 @@ function renderContent(data, opts ) {
         }
       });
     }
-
-    //feed_success(feed);
 
     $.ajax({
       url: feed,
