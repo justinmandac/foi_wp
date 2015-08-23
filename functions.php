be6f02b786228e2d36ad70b851bb44b7ecb7a0bd
unknown <justinmandac@gmail.com>
2015-08-23 11:31:24 +0800

    cleanup

--- functions.php
+++ functions.php
@@ -6,13 +6,9 @@ function register_main_menu() {
 }
 add_action( 'init', 'register_main_menu' );
 
-
-
-
 function load_scripts() {
 
   wp_enqueue_script('cdn-jquery', 'https://code.jquery.com/jquery-2.1.4.min.js','','2.1.4',true);
-
   wp_enqueue_style( 'prefix-font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), '4.4.0' );
 
   if(is_page('timeline')) {
@@ -23,15 +19,13 @@ function load_scripts() {
   }
 
   if(is_page('tracker')) {
-    wp_enqueue_style( 'sidr-style', get_stylesheet_directory_uri().'/stylesheets/jquery.sidr.light.css' );
     wp_enqueue_script('sidr', get_stylesheet_directory_uri().'/js/jquery.sidr.min.js','','3.0.3',true);
     wp_enqueue_script('xml2json', get_stylesheet_directory_uri().'/js/jquery.xml2json.js','','3.0.3',true);
     wp_enqueue_script('moment', get_stylesheet_directory_uri().'/js/moment.min.js','','1.0.0',true);
-    wp_enqueue_script('tracker', get_stylesheet_directory_uri().'/js/tracker.js','','1.0.0',true);
     wp_enqueue_script('highcharts', get_stylesheet_directory_uri().'/js/highcharts.js','','1.0.0',true);
     wp_enqueue_script('highcharts-more', get_stylesheet_directory_uri().'/js/highcharts-more.js','','1.0.0',true);
     wp_enqueue_script('tracker', get_stylesheet_directory_uri().'/js/tracker.js','','1.0.0',true);
-    wp_enqueue_style( 'timeline-style', get_stylesheet_directory_uri().'/stylesheets/tracker.css' );
+    wp_enqueue_style( 'tracker-styles', get_stylesheet_directory_uri().'/stylesheets/tracker-styles.css' );
   }
 
   wp_enqueue_script('app', get_stylesheet_directory_uri().'/js/app.js','','1.0.0',true);
